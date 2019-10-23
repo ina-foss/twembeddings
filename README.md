0f886ace3c4d8c03ffb4c2fb3dd1d766db13e099
bmaz <beatrice.mazoyer@gmail.com>
2019-10-23 15:26:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Sentence embeddings for unsupervised event detection in the Twitter stream
+
+This repo aims at letting researchers reproduce our Twitter event detection results on the 
+[Event2012 dataset](https://cs.adelaide.edu.au/~wei/sublinks/papers/2.1.2013CIKM.Building%20a%20Large-scale%20Corpus%20for%20Evaluating%20Event.pdf).
+Since some tweets may have probably been erased since we collected the dataset, we cannot ensure 100% identical results,
+ but we are confident that the comparative performance of the embeddings will remain unchanged.
+ 
+
+## Summary:
+
+* [Installation](#installation)
+* [Download Event2012 dataset](#download-event2012-dataset)
+* [Available embeddings](#available-embeddings)
+    * [Requirements](#requirements)
+    * [Submodules](#submodules)
+* [Citation](#citation)
+* [Acknowledgment](#acknowledgment)
+
+## Installation
+We recommand using Anaconda 3 to create a python 3.6 environment 
+(install Anaconda [here](https://docs.anaconda.com/anaconda/install/)):
+
+    conda create -n "twembeddings" python=3.6.9
+    source activate twembeddings
+    
+Then clone the repo and install the complementary requirements:
+
+    cd $HOME
+    git clone https://github.com/bmaz/twembeddings.git
+    cd twembeddings
+    pip install -r requirements.txt
+    
+## Download Event2012 dataset
+
+### Download IDs
+In compliance with Twitter terms of use, the authors of the dataset do not share the tweets content,
+but only the tweets IDs. Accept the 
+[dataset agreement](https://docs.google.com/forms/d/e/1FAIpQLSfRQX4R2O_Pv26wuepydKS4xxi6QbLrhaCgJaAXPcKx7dDljQ/viewform)
+and download the dataset. Untar the folder, the labeled tweets are in the `relevant_tweets.tsv` file. 
+
+### Download content
+We provide a script to download the content of the tweets from the Twitter API. In order to run the script,
+you need to [create a Twitter developper account and a Twitter App](https://developer.twitter.com/en/docs/basics/apps/overview).
+Then get the app's [access tokens](https://developer.twitter.com/en/docs/basics/authentication/guides/access-tokens).
+You should now have 4 tokens (the following strings are random examples):
+- app_key: mIsU1P0NNjUTf9DjuN6pdqyOF
+- app_secret: KAd5dpgRlu0X3yizTfXTD3lZOAkF7x0QAEhAMHpVCufGW4y0t0
+- oauth_token: 4087833385208874171-k6UR7OGNFdfBcqPye8ps8uBSSqOYXm
+- oauth_token_secret: Z9nZBVFHbIsU5WQCGT7ZdcRpovQm0QEkV4n4dDofpYAEK
+
+Run the script:
+
+    python get_tweets_objects.py \
+    --path /yourpath/relevant_tweets.tsv \
+    --app_key mIsU1P0NNjUTf9DjuN6pdqyOF \
+    --app_secret KAd5dpgRlu0X3yizTfXTD3lZOAkF7x0QAEhAMHpVCufGW4y0t0 \
+    --oauth_token 4087833385208874171-k6UR7OGNFdfBcqPye8ps8uBSSqOYXm \
+    --oauth_token_secret Z9nZBVFHbIsU5WQCGT7ZdcRpovQm0QEkV4n4dDofpYAEK
+
+The script may take some time to run entirely, since it respects the API's 
+[rate limit](https://developer.twitter.com/en/docs/basics/rate-limits).
---
+++ get_tweets_objects.py
@@ -0,0 +1,74 @@
+from twython import Twython, TwythonError, TwythonAuthError, TwythonRateLimitError
+import time
+import logging
+import json
+import pandas as pd
+import argparse
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO)
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+for argument in ['--path', '--app_key', '--app_secret', '--oauth_token', '--oauth_token_secret']:
+    parser.add_argument(argument, required=True)
+args = vars(parser.parse_args())
+
+def format_tweet(row):
+    tweet_dict = {}
+    tweet_dict["text"] = row["full_text"]
+    tweet_dict["id"] = row["id_str"]
+    tweet_dict["created_at"] = row["created_at"]
+    if "extended_entities" in row and "media" in row["extended_entities"]:
+        tweet_dict["url_image"] = row["extended_entities"]["media"][0]["media_url"]
+    return tweet_dict
+
+
+def rehydrate_tweets(twitter_obj, tweet_id_list, jsondump=False):
+    logging.info("Starting calls to Twitter API. This may take some time.")
+    tweets = []
+    id_count = 0
+    while True:
+        try:
+            batch = twitter_obj.lookup_status(
+                id=tweet_id_list[id_count:id_count+100],
+                include_entities=True,
+                tweet_mode="extended")
+        except TwythonRateLimitError:
+            reset_time = float(twitter_obj.get_lastfunction_header("x-rate-limit-reset"))
+            delta = round(int(reset_time) - time.time(), 0)
+            logging.info("Twitter rate limit reached, sleeping {} seconds".format(delta + 1))
+            time.sleep(delta + 1)
+            continue
+        if len(batch) == 0:
+            break
+        if jsondump:
+            with open("data/event_2018.json", "a+") as f:
+                for tweet in batch:
+                    json_str = json.dumps(tweet) + "\n"
+                    f.write(json_str)
+        tweets += batch
+        id_count += 100
+
+    # Check to see if we didn't get all of the requested tweets back
+    if (len(tweet_id_list) != len(tweets)):
+        logging.info("{}% of ids collected. Some tweets/accounts may have been deleted.".format(
+            round(100*len(tweets)/len(tweet_id_list), 0))
+        )
+
+    return tweets
+
+
+def main():
+    path = args.pop("path")
+    twitter_obj = Twython(**args)
+    labeled_data = pd.read_csv(path, sep="\t", header=None, names=["label", "id"]).drop_duplicates()
+    ids = labeled_data.id.tolist()
+    tweets = rehydrate_tweets(twitter_obj, ids, jsondump=True)
+    complete_data = pd.DataFrame([format_tweet(row) for row in tweets])
+    complete_data = labeled_data.merge(complete_data, on="id", how="left")
+    complete_data = complete_data[complete_data.text.notna()]
+    complete_data["label"] = complete_data["label"].astype(str).str.replace(".0", "").str.replace("nan", "")
+    complete_data["text"] = complete_data["text"].str.replace("\t", " ").str.replace("\n", " ").str.replace("\r", " ")
+    complete_data.to_csv("data/event2012_test.tsv", sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    main()
