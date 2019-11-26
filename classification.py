f459dd4c586b85df8b2a211f6129e476bcf0c183
bmaz <beatrice.mazoyer@gmail.com>
2019-11-26 15:23:55 +0100

    multiprocessing

--- classification.py
+++ classification.py
@@ -6,6 +6,9 @@ import pandas as pd
 import argparse
 import logging
 import yaml
+from multiprocessing import Pool
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO)
 text_embeddings = ['tfidf_dataset', 'tfidf_all_tweets', 'w2v_gnews_en', "elmo", "bert", "sbert_nli_sts", "use"]
@@ -42,35 +45,50 @@ def main(args):
                 if opt != "threshold":
                     params[opt] = options[model][opt]
                     logging.info("Param '{}' : {}".format(opt, options[model][opt]))
-        for seed in [42, 11, 1008, 2993, 559]:
-            test_params(**params, seed=seed)
+        test_params(**params, seeds=[42, 11, 1008, 2993, 559])
+
 
 def kernel(X, Y):
     return 1 - abs(euclidean_distances(X, Y))
+
+
+def classif(X_train, X_test, y_train, y_test, seed):
+    clf = SVC(kernel=kernel, C=3)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    precision, recall, f1, _ = precision_recall_fscore_support(y_test, y_pred, average='macro')
+    return {"p": precision, "r": recall, "f1": f1, "seed": seed}
+
 
 def test_params(**params):
     data = load_dataset(dataset=params["dataset"], annotation=params["annotation"], text=params["text+"])
     X = build_matrix(**params)
     y = data.label.astype(int).values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5, random_state=params["seed"])
-    clf = SVC(kernel=kernel, C=3)
-    logging.info("Start SVM classification. This may take some time...")
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    precision, recall, f1, _ = precision_recall_fscore_support(y_test, y_pred, average='macro')
+    splits = []
+    for seed in params.pop("seeds"):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5, random_state=seed)
+        splits.append((X_train, X_test, y_train, y_test, seed))
+
+    p = Pool(processes=4)
+    results = p.starmap(classif, splits)
     params["kernel"] = "triangular"
-    params["p"] = precision
-    params["r"] = recall
-    params["f1"] = f1
-    current_results = pd.DataFrame(params, index=[0])
-    logging.info(current_results[["seed", "model", "tfidf_weights", "p", "r", "f1", "kernel"]].iloc[0])
-    if params["save_results"]:
-        try:
-            results = pd.read_csv("results_classif.csv")
-        except FileNotFoundError:
-            results = pd.DataFrame()
-        current_results = results.append(current_results, ignore_index=True)
-        current_results.to_csv("results_classif.csv")
+    for result in results:
+        current_results = params.copy()
+        current_results.update(result)
+        current_results = pd.DataFrame(current_results, index=[0])
+        if params["save_results"]:
+            try:
+                results = pd.read_csv("results_classif.csv")
+            except FileNotFoundError:
+                results = pd.DataFrame()
+            current_results = results.append(current_results, ignore_index=True)
+            current_results.to_csv("results_classif.csv")
+    results = pd.DataFrame(results, index=range(len(results)))
+    mean = results[["f1", "p", "r"]].mean().round(2)
+    std = results[["f1", "p", "r"]].std().round(2)
+    logging.info("F1: {}±{}   precision: {}±{}   recall: {}±{}".format(
+        mean["f1"], std["f1"], mean["p"], std["p"], mean["r"], std["r"]
+    ))
 
 
 if __name__ == '__main__':
