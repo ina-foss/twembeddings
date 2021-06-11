d5485932f35951ef04f4d308a32309f963b2bd6c
Yomguithereal <guillaumeplique@gmail.com>
2021-06-11 11:34:55 +0200

    Indexing predictions

--- rust/src/cmd/evaluation.rs
+++ rust/src/cmd/evaluation.rs
@@ -1,6 +1,5 @@
 use std::boxed::Box;
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::error::Error;
 
 use clap::Clap;
@@ -31,7 +30,7 @@ pub fn run(cli_args: &Opts) -> Result<(), Box<dyn Error>> {
     let label_column_index = get_column_index(&headers, "label")?;
 
     let mut truth: HashMap<u64, usize> = HashMap::new();
-    let mut distinct_truth_labels: HashSet<usize> = HashSet::new();
+    let mut truth_clusters: HashMap<usize, Vec<u64>> = HashMap::new();
 
     for result in rdr.records() {
         bar.inc(1);
@@ -47,7 +46,8 @@ pub fn run(cli_args: &Opts) -> Result<(), Box<dyn Error>> {
 
         if let Some(label) = label_option {
             truth.insert(id, label);
-            distinct_truth_labels.insert(label);
+            let cluster = truth_clusters.entry(label).or_default();
+            cluster.push(id);
         }
     }
 
@@ -56,7 +56,47 @@ pub fn run(cli_args: &Opts) -> Result<(), Box<dyn Error>> {
     eprintln!(
         "Indexed {:?} labeled tweets as truth, arranged in {:?} clusters.",
         truth.len(),
-        distinct_truth_labels.len()
+        truth_clusters.len()
+    );
+
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(if cli_args.tsv { b'\t' } else { b',' })
+        .from_path(&cli_args.predicted)?;
+
+    let headers = rdr.headers()?;
+
+    let id_column_index = get_column_index(&headers, "id")?;
+    let thread_column_index = get_column_index(&headers, "thread_id")?;
+
+    let bar = acquire_progress_indicator("Processing predictions", cli_args.total);
+
+    let mut predicted: HashMap<u64, usize> = HashMap::new();
+    let mut predicted_clusters: HashMap<usize, Vec<u64>> = HashMap::new();
+
+    for result in rdr.records() {
+        bar.inc(1);
+
+        let record = result?;
+
+        let id: u64 = record[id_column_index].parse()?;
+        let thread_id: usize = record[thread_column_index].parse()?;
+
+        // We don't consider extraneous tweets
+        if !truth.contains_key(&id) {
+            continue;
+        }
+
+        predicted.insert(id, thread_id);
+        let cluster = predicted_clusters.entry(thread_id).or_default();
+        cluster.push(id);
+    }
+
+    bar.finish_at_current_pos();
+
+    eprintln!(
+        "Indexed {:?} tweets as predictions, arranged in {:?} clusters.",
+        predicted.len(),
+        predicted_clusters.len()
     );
 
     Ok(())
