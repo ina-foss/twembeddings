962ed8e0eb8cd444640e1aea1fea97daf23ab313
bmaz <beatrice.mazoyer@gmail.com>
2021-06-15 16:36:33 +0200

    idf threshold

--- rust/src/vectorization.rs
+++ rust/src/vectorization.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+const IDF_THRESHOLD: f64 = 0.21;
+
 /// Transform tokens into a sparse normalized vector represented as tuples (dim, idf)
 /// and sorted by idf so that more discriminant dimensions appear first.
 pub fn vectorize(
@@ -8,17 +10,24 @@ pub fn vectorize(
 ) -> Vec<(usize, f64)> {
     let mut vector: Vec<(usize, f64)> = Vec::new();
     let mut norm = 0.0;
+    let mut is_relevant = false;
 
     for token in tokens {
         match vocabulary.get(token) {
             Some((dim, w)) => {
                 norm += w * w;
                 vector.push((*dim, *w));
+                if *w >= IDF_THRESHOLD {
+                    is_relevant = true;
+                }
             }
             None => continue,
         }
     }
 
+    if !is_relevant {
+        return Vec::new();
+    }
     // Normalizing the vector
     norm = norm.sqrt();
 
@@ -29,8 +38,4 @@ pub fn vectorize(
     vector.sort_unstable_by(|x, y| (y.1, y.0).partial_cmp(&(x.1, x.0)).unwrap());
 
     vector
-}
-
-pub fn filter_irrelevant_vector(vector: &[(usize, f64)], threshold: f64) -> bool {
-    vector.iter().all(|(_, w)| *w < threshold)
 }
