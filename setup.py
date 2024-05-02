9a24a3287086eeb57fa53bcaf00c8fdb12443f76
bmaz <beatrice.mazoyer@gmail.com>
2024-05-02 16:46:05 +0200

    Bump 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='twembeddings',
-      version='0.1',
+      version='0.2.0',
       description='event detection in tweets',
       url='https://github.com/ina-foss/twembeddings',
       author='Béatrice Mazoyer',
